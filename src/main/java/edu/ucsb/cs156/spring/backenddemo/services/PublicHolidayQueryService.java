f7b881766e796997c51a05fa5a0c16088b2f5eb5
Shuyi Wan <shuyiwan@ucsb.edu>
2023-10-19 13:02:58 -0700

    shuyi added service to get information about year and countryCode

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -8,6 +8,24 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
 
 @Slf4j
 @Service
@@ -22,13 +40,13 @@ public class PublicHolidayQueryService {
 
     public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
 
-    public String getJSON(String date, String localName, String name, String countryCode, boolean fixed, boolean global, String[] counties, int launchYear, String type) throws HttpClientErrorException {
-        log.info("data={}, localName={}, name={}, countryCode={}, fixed={}, global={}, counties={}, launchYear={}, type={}", data, localName, name, countryCode, fixed, global, counties, launchYear, type);
+    public String getJSON(String year, String countryCode) throws HttpClientErrorException {
+        log.info("year={}, countryCode={}", year, countryCode);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        Map<String, String> uriVariables = Map.of("country", country);
+        Map<String, String> uriVariables = Map.of("year", year, "countryCode", countryCode);
 
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
 

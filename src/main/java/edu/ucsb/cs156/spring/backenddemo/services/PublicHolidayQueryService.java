dbbe0230641c761309f4ae63fa39bd33bf49343f
Shuyi Wan <shuyiwan@ucsb.edu>
2023-10-18 17:18:03 -0700

    Shuyi - define ENDPOINT for holiday service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -13,16 +13,28 @@ import org.springframework.web.client.HttpClientErrorException;
 @Service
 public class PublicHolidayQueryService {
 
-
+    ObjectMapper mapper = new ObjectMapper();
     private final RestTemplate restTemplate;
 
     public PublicHolidayQueryService(RestTemplateBuilder restTemplateBuilder) {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
 
-    public String getJSON(String year, String countryCode) throws HttpClientErrorException {
+    public String getJSON(String date, String localName, String name, String countryCode, boolean fixed, boolean global, String[] counties, int launchYear, String type) throws HttpClientErrorException {
+        log.info("data={}, localName={}, name={}, countryCode={}, fixed={}, global={}, counties={}, launchYear={}, type={}", data, localName, name, countryCode, fixed, global, counties, launchYear, type);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> uriVariables = Map.of("country", country);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
+                uriVariables);
+        return re.getBody();
         return "";
     }
 

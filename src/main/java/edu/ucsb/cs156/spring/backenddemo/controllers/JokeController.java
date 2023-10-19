d8e759463b099015d7acee388d39f6f2c7eee31b
David Wang <david.wang5766@gmail.com>
2023-10-18 23:58:14 -0700

    Adding controller and tests for Joke Query Service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeController.java
@@ -1,8 +1,35 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ucsb.cs156.spring.backenddemo.services.JokeQueryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Operation;
+
+@Tag(name = "Jokes from https://v2.jokeapi.dev/joke/")
 @RestController
+@RequestMapping("/api/jokes")
 public class JokeController {
-    
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    JokeQueryService jokeQueryService;
+
+    @Operation(summary = "Gets jokes about various topics")
+    @GetMapping("/get")
+    public ResponseEntity<String> getJokes(
+            @Parameter(name = "category", example = "Programming") @RequestParam String category,
+            @Parameter(name = "number of jokes", example = "2") @RequestParam Integer numJokes)
+            throws JsonProcessingException {
+        String result = jokeQueryService.getJSON(category, numJokes);
+        return ResponseEntity.ok().body(result);
+    }
 }
